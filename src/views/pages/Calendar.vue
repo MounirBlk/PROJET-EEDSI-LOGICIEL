73087c24ffd1db13b57e4e994c105269bbe5eb61
MounirBlk <mounir.ballouk@gmail.com>
2021-04-20 23:36:27 +0200

    👌 IMPROVE: calendrier de commandes

--- src/views/pages/Calendar.vue
+++ src/views/pages/Calendar.vue
@@ -1,17 +1,62 @@
 <template>
-<v-container id="calendar">
+<v-container id="calendar" fluid tag="section">
+    <v-overlay :absolute="isAbsolute" :opacity="opacity" :value="isOverlay">
+        <v-progress-circular color="red" indeterminate size="80"></v-progress-circular>
+    </v-overlay>
+    <v-dialog v-model="isDialogEvent" width="500" overlay-opacity="0.8">
+        <v-card outlined>
+            <v-card-title>
+                Assigner une livraison
+            </v-card-title>
+            <v-card-text>
+                <v-simple-table dense>
+                    <template v-slot:default>
+                        <tbody>
+                            <tr>
+                                <td>Statut</td>
+                                <td>{{ infoEvenement.statut }}</td>
+                            </tr>
+                            <tr>
+                                <td>Adresse de livraison</td>
+                                <td>{{ infoEvenement.adresseLivraison }}</td>
+                            </tr>
+                            <tr>
+                                <td>Début</td>
+                                <td>{{ infoEvenement.start | moment("YYYY-MM-DD HH:mm") }}</td>
+                            </tr>
+                            <tr>
+                                <td>Fin</td>
+                                <td>{{ infoEvenement.end | moment("YYYY-MM-DD HH:mm") }}</td>
+                            </tr>
+                        </tbody>
+                    </template>
+                </v-simple-table>
+                <div class="mt-4">
+                    <v-chip v-if="infoEvenement.livreur !== null && infoEvenement.livreur !== undefined">Livreur: M/Mme. {{ infoEvenement.livreur.lastname }}</v-chip>
+                    <v-autocomplete v-else class="mt-5" :items="livreurs" prepend-icon="mdi-magnify" color="red" item-text="lastname" item-value="_id" v-model="livreurID" hide-no-data hide-details item-color label="Livreur" outlined></v-autocomplete>
+                </div>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="mr-1" color="error" text @click.prevent="isDialogEvent = false;">Fermer</v-btn>
+                <v-btn color="success" text :disabled="infoEvenement.livreur !== null && infoEvenement.livreur !== undefined" @click.prevent="saveAssignage(infoEvenement, livreurID)">
+                    <v-icon left>mdi-check</v-icon> Sauvegarder
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
     <v-row>
         <v-col cols="12" md="12" sm="12">
-            <base-material-card icon="mdi-calendar-month-outline" color="error" max-width="100%" width="auto" class="px-5 py-3 mx-auto">
+            <base-material-card icon="mdi-calendar-month-outline" color="red" max-width="100%" width="auto" class="px-5 py-3 mx-auto">
                 <template v-slot:after-heading>
-                    <div class="display-1 font-weight-light">Calendrier de livraisons</div>
+                    <div class="display-1 font-weight-light">Calendrier de commandes</div>
                 </template>
                 <v-row>
                     <v-col cols="12" md="12">
                         <v-card>
                             <v-toolbar flat>
-                                <v-toolbar-title v-if="$vuetify.breakpoint.mdAndUp">{{ new Date().toISOString().substr(0, 10) }}</v-toolbar-title>
-                                <v-btn color="error" icon class="ml-3">
+                                <v-toolbar-title v-if="$vuetify.breakpoint.mdAndUp">{{ new Date() | moment("YYYY-MM-DD HH:mm") }}</v-toolbar-title>
+                                <v-btn color="red" icon class="ml-3" @click="getData">
                                     <v-icon large>mdi-refresh</v-icon>
                                 </v-btn>
                             </v-toolbar>
@@ -22,15 +67,15 @@
                     <v-col>
                         <v-sheet height="64">
                             <v-toolbar flat>
-                                <v-btn outlined class="mr-4" color="error darken-2" @click="setToday">
+                                <v-btn outlined class="mr-4" color="red darken-2" @click="focus = ''">
                                     Today
                                 </v-btn>
-                                <v-btn fab text small color="error darken-2" @click="prev">
+                                <v-btn fab text small color="red darken-2" @click="$refs.calendar.prev()">
                                     <v-icon small>
                                         mdi-chevron-left
                                     </v-icon>
                                 </v-btn>
-                                <v-btn fab text small color="error darken-2" @click="next">
+                                <v-btn fab text small color="red darken-2" @click="$refs.calendar.next()">
                                     <v-icon small>
                                         mdi-chevron-right
                                     </v-icon>
@@ -41,7 +86,7 @@
                                 <v-spacer></v-spacer>
                                 <v-menu bottom right>
                                     <template v-slot:activator="{ on, attrs }">
-                                        <v-btn outlined color="error darken-2" v-bind="attrs" v-on="on">
+                                        <v-btn outlined color="red darken-2" v-bind="attrs" v-on="on">
                                             <span>{{ typeToLabel[type] }}</span>
                                             <v-icon right>
                                                 mdi-menu-down
@@ -66,53 +111,38 @@
                             </v-toolbar>
                         </v-sheet>
                         <v-sheet height="600">
-                            <v-calendar ref="calendar" v-model="focus" color="primary" :events="events" :event-color="getEventColor" :type="type" @click:event="showEvent" @click:more="viewDay" @click:date="viewDay" @change="updateRange"></v-calendar>
-                            <v-menu v-model="selectedOpen" :close-on-content-click="false" :activator="selectedElement" offset-x>
-                                <v-card min-width="350px" flat>
-                                    <v-toolbar :color="selectedEvent.color">
-                                        <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                                    </v-toolbar>
-                                    <v-card-text>
-                                        <span v-html="selectedEvent.details"></span>
-                                        <v-select :color="selectedEvent.color" prepend-inner-icon="mdi-format-list-bulleted-type" :items="['Livreur1 livreur1', 'Livreur2 livreur2']" label="Livreur*"></v-select>
-
-                                    </v-card-text>
-                                    <v-card-actions>
-                                        <v-btn text color="error" @click="selectedOpen = false">
-                                            Cancel
-                                        </v-btn>
-                                        <v-spacer></v-spacer>
-                                        <v-btn text color="success" @click="selectedOpen = false">
-                                            Valider
-                                        </v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-menu>
+                            <v-calendar ref="calendar" v-model="focus" color="primary" :events="events" :event-color="getEventColor" :type="type" @click:event="myEvent" @click:more="viewDay" @click:date="viewDay"></v-calendar>
                         </v-sheet>
                     </v-col>
                 </v-row>
             </base-material-card>
         </v-col>
     </v-row>
+    <v-snackbar v-model="isSnackbarOpened" elevation="24" :color="isSuccess ? 'success' : 'error'">
+        <div class="text-center subtitle-1">
+            <v-icon v-if="!isSuccess" color="white" left>mdi-alert-outline</v-icon>
+            <v-icon v-else color="white" left>mdi-checkbox-marked-circle-outline</v-icon>
+            <span>{{ snackbarMessage }}</span>
+            <v-btn dark icon class="ml-6" @click="isSnackbarOpened = false">
+                <v-icon>mdi-close</v-icon>
+            </v-btn>
+        </div>
+    </v-snackbar>
 </v-container>
 </template>
 
 <script lang="ts">
-import Vue, {
-    VNode
-} from 'vue';
-import {
-    bus
-} from "../../main";
-import axiosApi from "../../plugins/axiosApi";
+import Vue from 'vue';
+import Gestion from "../../mixins/Gestion"
+import axiosApi from '../../plugins/axiosApi';
 import qs from "qs";
 import {
     AxiosResponse
 } from 'axios';
-import Gestion from "../../mixins/Gestion"
+import moment from 'moment';
 
 export default Vue.extend({
-    name: 'Example',
+    name: 'Calendrier',
     mixins: [Gestion],
     props: {},
     components: {},
@@ -122,9 +152,9 @@ export default Vue.extend({
             focus: '',
             type: 'month',
             typeToLabel: {
-                month: 'Month',
-                week: 'Week',
-                day: 'Day',
+                'month': 'Month',
+                'week': 'Week',
+                'day': 'Day',
                 '4day': '4 Days',
             },
             selectedEvent: {},
@@ -132,21 +162,78 @@ export default Vue.extend({
             selectedOpen: false,
             events: [],
             colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-            names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+            commandes: [],
+            livreurs: [],
+            infoEvenement: {},
+            isDialogEvent: false,
+            livreurID: null
         }
     },
     computed: {},
     watch: {},
-    created() {
-        //console.log('created')
-    },
-    beforeMount() {
-        //console.log('beforeMount')
-    },
-    mounted() {
-        this.$refs.calendar.checkChange()
+    created() {},
+    beforeMount() {},
+    async mounted() {
+        await this.getData()
     },
     methods: {
+        getData: function (): void {
+            this.isOverlay = true;
+            const requestCommandes = axiosApi.get('/commande/all/all');
+            const requestLivreurs = axiosApi.get('/user/all/Livreur');
+            Promise.all([requestCommandes, requestLivreurs])
+                .then((response: AxiosResponse[]) => {
+                    Object.assign(this.$data, this.$options.data()); //reset data
+                    this.commandes = response[0].data.commandes;
+                    this.livreurs = response[1].data.users;
+                    this.commandes.forEach((element: any) => {
+                        this.events.push({
+                            idCommande: element._id,
+                            statut: element.statut,
+                            name: element.statut === "Attente" ? "En attente" : element.statut === "Livraison" ? "En cours de livraison" : element.statut === "Signalement" ? "Livraison en signalement" : element.statut === "Termine" ? "Livraison terminé" : "Commande",
+                            start: new Date(element.dateLivraison),
+                            end: new Date(element.dateLivraison),
+                            color: this.colors[this.rnd(0, this.colors.length - 1)],
+                            timed: true,
+                            adresseLivraison: element.adresseLivraison,
+                            livreur: element.livreurID
+                        })
+                    });
+                    this.$refs.calendar.checkChange()
+                    setTimeout(() => {
+                        this.isOverlay = false;
+                    }, 1000);
+                }).catch((error: any) => {
+                    this.catchAxios(error)
+                    setTimeout(() => {
+                        this.isOverlay = false;
+                    }, 1000);
+                });
+        },
+        saveAssignage: function (infoEvenement: any, livreurID: string) {
+            if (livreurID === '' || livreurID === null || livreurID === undefined) return this.errorMessage("Veuillez sélectionner un livreur !");
+            axiosApi.put("/commande/update/" + infoEvenement.idCommande, qs.stringify({
+                    livreurID: livreurID,
+                    statut: "Livraison"
+                })) //update
+                .then((response: AxiosResponse) => {
+                    Object.assign(this.$data, this.$options.data()); //reset data
+                    this.successMessage("L'assignage a bien été effectué !");
+                    setTimeout(() => {
+                        this.getData();
+                    }, 1000);
+                })
+                .catch((error) => {
+                    this.catchAxios(error)
+                    setTimeout(() => {
+                        this.isOverlay = false;
+                    }, 1000);
+                });
+        },
+        myEvent(day: any) {
+            this.infoEvenement = day.eventParsed.input;
+            this.isDialogEvent = true;
+        },
         viewDay({
             date
         }: any) {
@@ -155,65 +242,6 @@ export default Vue.extend({
         },
         getEventColor(event: any) {
             return event.color
-        },
-        setToday() {
-            this.focus = ''
-        },
-        prev() {
-            this.$refs.calendar.prev()
-        },
-        next() {
-            this.$refs.calendar.next()
-        },
-        showEvent({
-            nativeEvent,
-            event
-        }: any) {
-            const open = () => {
-                this.selectedEvent = event
-                this.selectedElement = nativeEvent.target
-                setTimeout(() => {
-                    this.selectedOpen = true
-                }, 10)
-            }
-
-            if (this.selectedOpen) {
-                this.selectedOpen = false
-                setTimeout(open, 10)
-            } else {
-                open()
-            }
-
-            nativeEvent.stopPropagation()
-        },
-        updateRange({
-            start,
-            end
-        }: any) {
-            const events = []
-
-            const min = new Date(`${start.date}T00:00:00`)
-            const max = new Date(`${end.date}T23:59:59`)
-            const days = (max.getTime() - min.getTime()) / 86400000
-            const eventCount = this.rnd(days, days + 20)
-
-            for (let i = 0; i < eventCount; i++) {
-                const allDay = this.rnd(0, 3) === 0
-                const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-                const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-                const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-                const second = new Date(first.getTime() + secondTimestamp)
-
-                events.push({
-                    name: this.names[this.rnd(0, this.names.length - 1)],
-                    start: first,
-                    end: second,
-                    color: this.colors[this.rnd(0, this.colors.length - 1)],
-                    timed: !allDay,
-                })
-            }
-
-            this.events = events
         },
         rnd(a: number, b: number) {
             return Math.floor((b - a + 1) * Math.random()) + a
