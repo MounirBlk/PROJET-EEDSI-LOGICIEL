123d8a514a1d465127305b0f2ffbbaa10af6ba4c
MounirBlk <mounir.ballouk@gmail.com>
2021-05-21 15:33:22 +0200

    👌 IMPROVE: animation

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -2,24 +2,26 @@
 <v-card class="mx-auto px-3" max-width="auto" tile>
     <v-row>
         <v-col cols="12" md="12" sm="12">
-            <v-list-item class="mt-n7" two-line v-for="(item, index) in paginatedData" :key="index">
-                <v-list-item-avatar v-ripple :color="item.userInfos.role !== 'Administrateur' ? 'indigo' : 'orange'">
-                    <span class="white--text">{{ item.userInfos.firstname.charAt(0).toUpperCase() }}{{ item.userInfos.lastname.charAt(0).toUpperCase() }}</span>
-                </v-list-item-avatar>
-                <v-list-item-content class="mt-5">
-                    <v-card max-width="250" :color="item.userInfos.role !== 'Administrateur' ? 'rgba(63, 81, 181, .8)' : 'rgba(255, 152, 0, .8)'" v-ripple class="white--text py-1 px-2 mb-1">
-                        <v-divider class="mb-1" color="transparent"></v-divider>
-                        <v-list-item-title :class="item.userInfos.role !== 'Administrateur' ? 'grey--text subtitle-2 mb-1' : 'brown--text subtitle-2 mb-1'">
-                            <v-icon left small v-if="item.userInfos.role === 'Administrateur'">mdi-star</v-icon>
-                            <v-icon left small v-else>mdi-account</v-icon>{{ item.username }}
-                        </v-list-item-title>
-                        <!--<v-list-item-subtitle class="white--text subtitle-1">{{ item.message }}</v-list-item-subtitle>-->
-                        <span>{{ item.message }}</span>
-                        <v-divider class="mt-1" color="transparent"></v-divider>
-                    </v-card>
-                    <v-list-item-subtitle class="mx-2 caption grey--text">{{ item.createdAt | moment("YYYY/MM/DD HH:mm") }}</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
+            <sequential-entrance delay="250">
+                <v-list-item class="mt-n7" two-line v-for="(item, index) in paginatedData" :key="index">
+                    <v-list-item-avatar v-ripple :color="item.userInfos.role !== 'Administrateur' ? 'indigo' : 'orange'">
+                        <span class="white--text">{{ item.userInfos.firstname.charAt(0).toUpperCase() }}{{ item.userInfos.lastname.charAt(0).toUpperCase() }}</span>
+                    </v-list-item-avatar>
+                    <v-list-item-content class="mt-5">
+                        <v-card max-width="250" :color="item.userInfos.role !== 'Administrateur' ? 'rgba(63, 81, 181, .8)' : 'rgba(255, 152, 0, .8)'" v-ripple class="white--text py-1 px-2 mb-1">
+                            <v-divider class="mb-1" color="transparent"></v-divider>
+                            <v-list-item-title :class="item.userInfos.role !== 'Administrateur' ? 'grey--text subtitle-2 mb-1' : 'brown--text subtitle-2 mb-1'">
+                                <v-icon left small v-if="item.userInfos.role === 'Administrateur'">mdi-star</v-icon>
+                                <v-icon left small v-else>mdi-account</v-icon>{{ item.username }}
+                            </v-list-item-title>
+                            <!--<v-list-item-subtitle class="white--text subtitle-1">{{ item.message }}</v-list-item-subtitle>-->
+                            <span>{{ item.message }}</span>
+                            <v-divider class="mt-1" color="transparent"></v-divider>
+                        </v-card>
+                        <v-list-item-subtitle class="mx-2 caption grey--text">{{ item.createdAt | moment("YYYY/MM/DD HH:mm") }}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </sequential-entrance>
         </v-col>
         <v-col cols="12" md="12" sm="12" class="pa-0">
             <v-pagination circle color="indigo" v-model="pageNumber" :length="Math.ceil(messages.length/size)" prev-icon="mdi-menu-left" next-icon="mdi-menu-right" total-visible="5" @input="nextPage" />
--- src/views/core/Drawer.vue
+++ src/views/core/Drawer.vue
@@ -1,45 +1,49 @@
 <template>
 <v-navigation-drawer :src="!$vuetify.theme.dark ? barImageJour : barImageNuit" v-model="drawer" app id="core-navigation-drawer">
-    <center class="mt-5">
-        <v-avatar color="teal" size="50">
-            <img alt="Avatar" :src="!$vuetify.theme.dark ? 'https://png.pngtree.com/element_our/20200610/ourlarge/pngtree-shopping-mall-logo-image_2235997.jpg' : require('@/assets/logo.jpg')">
-        </v-avatar>
-        <strong class="ml-1">Dashboard E-Commerce </strong>
-    </center>
+    <sequential-entrance>
+        <center class="mt-5">
+            <v-avatar color="teal" size="50">
+                <img alt="Avatar" :src="!$vuetify.theme.dark ? 'https://png.pngtree.com/element_our/20200610/ourlarge/pngtree-shopping-mall-logo-image_2235997.jpg' : require('@/assets/logo.jpg')">
+            </v-avatar>
+            <strong class="ml-1">Dashboard E-Commerce </strong>
+        </center>
+    </sequential-entrance>
     <v-divider class="my-2"></v-divider>
     <v-list class="py-0" v-for="(item, index) in items" :key="index" v-if="item.disabled !== true">
-        <v-list-item dense link :to="item.to" v-if="!item.group">
-            <v-list-item-action>
-                <v-icon v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white">{{ item.icon }}</v-icon>
-                <v-icon v-else>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-                <v-list-item-title>
-                    <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ item.title }}</strong>
-                    <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ item.title }}</strong>
-                </v-list-item-title>
-            </v-list-item-content>
-        </v-list-item>
-        <v-list-group v-else :color="!$vuetify.theme.dark ? 'black' : 'white'" :value="true" :prepend-icon="item.icon">
-            <template v-slot:activator>
-                <v-list-item-title>
-                    <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ item.title }}</strong>
-                    <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ item.title }}</strong>
-                </v-list-item-title>
-            </template>
-            <v-list-item dense class="ml-3 mt-1" v-for="(child, i) in item.children" :to="child.to" :key="i" link>
+        <sequential-entrance>
+            <v-list-item dense link :to="item.to" v-if="!item.group">
                 <v-list-item-action>
-                    <v-icon v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white">{{ child.icon }}</v-icon>
-                    <v-icon v-else>{{ child.icon }}</v-icon>
+                    <v-icon v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white">{{ item.icon }}</v-icon>
+                    <v-icon v-else>{{ item.icon }}</v-icon>
                 </v-list-item-action>
                 <v-list-item-content>
                     <v-list-item-title>
-                        <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ child.title }}</strong>
-                        <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ child.title }}</strong>
+                        <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ item.title }}</strong>
+                        <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ item.title }}</strong>
                     </v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-        </v-list-group>
+            <v-list-group v-else :color="!$vuetify.theme.dark ? 'black' : 'white'" :prepend-icon="item.icon">
+                <template v-slot:activator>
+                    <v-list-item-title>
+                        <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ item.title }}</strong>
+                        <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ item.title }}</strong>
+                    </v-list-item-title>
+                </template>
+                <v-list-item dense class="ml-3 mt-1" v-for="(child, i) in item.children" :to="child.to" :key="i" link>
+                    <v-list-item-action>
+                        <v-icon v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white">{{ child.icon }}</v-icon>
+                        <v-icon v-else>{{ child.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            <strong v-if="!$vuetify.theme.dark" style="text-shadow: 0.1em 0.1em 0.1em white; font-size: 18px">{{ child.title }}</strong>
+                            <strong v-else style="text-shadow: 0.1em 0.1em 0.1em black; font-size: 18px">{{ child.title }}</strong>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+        </sequential-entrance>
     </v-list>
 </v-navigation-drawer>
 </template>
@@ -55,53 +59,52 @@ export default Vue.extend({
         barImageNuit: "https://www.euractiv.fr/wp-content/uploads/sites/3/2019/07/shutterstock_233084350-800x450.jpg",
         name: 'Dashboard',
         items: [{
-                icon: 'mdi-home-outline',
-                title: 'Accueil',
-                to: '/',
+            icon: 'mdi-home-outline',
+            title: 'Accueil',
+            to: '/',
+        }, {
+            title: 'Calendrier',
+            icon: 'mdi-calendar-month-outline',
+            to: '/calendar',
+        }, {
+            group: true,
+            icon: 'mdi-account-group-outline',
+            title: 'Utilisateurs',
+            children: [{
+                title: 'Commerciaux',
+                icon: 'mdi-account-tie-outline',
+                to: '/utilisateurs',
             }, {
-                title: 'Calendrier',
-                icon: 'mdi-calendar-month-outline',
-                to: '/calendar',
+                title: 'Clients',
+                icon: 'mdi-clipboard-account-outline',
+                to: '/clients',
             }, {
-                group: true,
-                icon: 'mdi-account-group-outline',
-                title: 'Utilisateurs',
-                children: [{
-                    title: 'Commerciaux',
-                    icon: 'mdi-account-tie-outline',
-                    to: '/utilisateurs',
-                }, {
-                    title: 'Clients',
-                    icon: 'mdi-clipboard-account-outline',
-                    to: '/clients',
-                }, {
-                    title: 'Livreurs',
-                    icon: 'mdi-walk',
-                    to: '/livreurs',
-                }]
-            },{
-                title: 'Factures',
-                icon: 'mdi-receipt',
-                to: '/factures',
-                disabled: true
-            }, {
-                title: 'Produits',
-                icon: 'mdi-sofa-single',
-                to: '/produits',
-            },{
-                title: 'Commandes',
-                icon: 'mdi-package-variant-closed',
-                to: '/commandes',
-            }, {
-                title: 'Paramètres',
-                icon: 'mdi-cogs',
-                to: '/parametres',
-            }, {
-                title: 'Prospections',
-                icon: 'mdi-bank',
-                to: '/prospections',
-            }
-        ]
+                title: 'Livreurs',
+                icon: 'mdi-walk',
+                to: '/livreurs',
+            }]
+        }, {
+            title: 'Factures',
+            icon: 'mdi-receipt',
+            to: '/factures',
+            disabled: true
+        }, {
+            title: 'Produits',
+            icon: 'mdi-sofa-single',
+            to: '/produits',
+        }, {
+            title: 'Commandes',
+            icon: 'mdi-package-variant-closed',
+            to: '/commandes',
+        }, {
+            title: 'Paramètres',
+            icon: 'mdi-cogs',
+            to: '/parametres',
+        }, {
+            title: 'Prospections',
+            icon: 'mdi-bank',
+            to: '/prospections',
+        }]
     }),
     computed: {
         drawer: {
